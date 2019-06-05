fab5c8399e295e3e14f749d2ef802d87339629b9
ZengJiFa <1104642947@qq.com>
2019-06-05 20:46:24 +0800

    Initial commit

---
+++ src/main/java/com/qianfeng/springbootgit/Controller/MyController.java
@@ -0,0 +1,14 @@
+package com.qianfeng.springbootgit.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author ZengJiFA
+ * @Date 2019/6/5
+ */
+@Controller
+@RequestMapping("my")
+public class MyController {
+
+}
